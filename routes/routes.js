a989404499243f54fabb76a3cc2c726e2e8160ad
Qazi Umer <qaziumer16@gmail.com>
2016-09-26 15:24:43 +0500

    errors2

--- routes/routes.js
+++ routes/routes.js
@@ -8,15 +8,15 @@ module.exports = function(express, app) {
         res.render("index", {});
     });
  
-    // router.get("/whoami", function(req, res, next) {
-    //     //   console.log ( "Platform" +  platform.os);
-    //     ipInfo((err, cLoc) => {
-    //         obj.IP = err || cLoc.ip;
-    //     });
-    //     obj.software = platform.os;
-    //     obj.language = req.acceptsLanguages('en-US', 'uk', 'ru-mo', 'be');
-    //     res.send(obj);
-    // });
+    router.get("/whoami", function(req, res, next) {
+        //   console.log ( "Platform" +  platform.os);
+        ipInfo((err, cLoc) => {
+            obj.IP = err || cLoc.ip;
+        });
+        obj.software = platform.os;
+        obj.language = req.acceptsLanguages('en-US', 'uk', 'ru-mo', 'be');
+        res.send(obj);
+    });
 
     app.use("/", router);
 }
